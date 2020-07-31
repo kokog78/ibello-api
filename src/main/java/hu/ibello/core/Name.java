8371d297555fbcac9e3efd9594223c33fda4ca58
Simon Kornél <simon.kornel@mikrum.hu>
2020-07-31 11:48:07 +0200

    @Name and @Description javadooc change

--- src/main/java/hu/ibello/core/Name.java
+++ src/main/java/hu/ibello/core/Name.java
@@ -26,20 +26,30 @@ import java.lang.annotation.Target;
 
 /**
  * <p>
- * With this annotation we can assign visible name to a class (step library) or a method
- * (test step). If the annotation presents on a class or method, then <em>ibello</em>
+ * With this annotation we can assign visible name to a class (step library or test data class) or a method
+ * (test step) or an enum constant (test data enum value) or a package (test data group).
+ * If the annotation presents on a class or method, then <em>ibello</em>
  * will log the name specified by the annotation when the class/method name is going to be
  * logged.
  * </p>
  * <p>
- * The value may contain parameter substitution markers. A parameter substitution marker
+ * If the annotation is added to a method, the value may contain parameter substitution markers. A parameter substitution marker
  * is a 0-based index surrounded by <code>${</code> and <code>}</code>. These markers will
  * be replaced by string version of method parameters. For example, if the annotation's value
  * is <code>"${0}-${1}"</code>, then the method will replace <code>${0}</code> with the first
  * method parameter, and <code>${1}</code> with the second parameter. If method parameters are
  * <code>"a"</code> and <code>1</code>, then the result will be <code>"a-1"</code>.
  * </p>
+ * <p>
+ * Test data classes can be named with this annotation. The name will be displayed by the <em>ibello</em> UI only.
+ * Packages containing test data classes may have this annotation too - in this case they define a group for
+ * the test data classes.
+ * </p>
+ * <p>
+ * Enum constants used by test data classes also can be named. The name will be displayed by te <em>ibello</em> UI only.
+ * </p>
  * @author Kornél Simon
+ * @see Description
  */
 @Retention(RUNTIME)
 @Target({METHOD, TYPE, FIELD, PACKAGE})
