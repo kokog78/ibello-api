de8890f0c5b0cc041f1d2c7e5e6d64efa7715e19
Kornél Simon <kokog.private@gmail.com>
2026-04-28 19:41:36 +0200

    new function object

---
+++ src/main/java/hu/ibello/functions/ExponentialFunction.java
@@ -0,0 +1,99 @@
+/*
+ * Ark-Sys Kft. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package hu.ibello.functions;
+
+/**
+ * A power function.
+ * <pre>
+ * f(x) = a * x^b
+ * </pre>
+ * Parameters are:
+ * <ul>
+ * <li>a</li>
+ * <li>b</li>
+ * </ul>
+ * @author kokog
+ *
+ */
+public class ExponentialFunction implements Function {
+
+	public ExponentialFunction() {
+	}
+	
+	public ExponentialFunction(double a, double b) {
+		this.a = a;
+		this.b = b;
+	}
+	
+	private double a;
+	private double b;
+	
+	public double getA() {
+		return a;
+	}
+	
+	public void setA(double a) {
+		this.a = a;
+	}
+	
+	public double getB() {
+		return b;
+	}
+	
+	public void setB(double b) {
+		this.b = b;
+	}
+	
+	@Override
+	public double value(double x) {
+		return a * Math.exp(b * x);
+	}
+
+	@Override
+	public int getParameterCount() {
+		return 2;
+	}
+
+	@Override
+	public double getParameter(int paramIndex) {
+		switch (paramIndex) {
+		case 0:
+			return a;
+		case 1:
+			return b;
+		}
+		throw new IllegalArgumentException("Unknown parameter index: " + paramIndex);
+	}
+
+	@Override
+	public void setParameter(int paramIndex, double value) {
+		switch (paramIndex) {
+		case 0:
+			a = value;
+			break;
+		case 1:
+			b = value;
+			break;
+		default:
+			throw new IllegalArgumentException("Unknown parameter index: " + paramIndex);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s * exp(%s * x)", getFormattedParameter(0), getFormattedParameter(1));
+	}
+}
